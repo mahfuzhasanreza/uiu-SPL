9a895677f1e6522349f26c1d3c8b57e064a29dad
mahfuzhr <mahfuzhasanreza1@gmail.com>
2024-12-16 15:54:56 +0600

    added 5(a) solution | (not varify)

--- Mid (Theory) Q. Solve/Spring 2023/Q5_a.c
+++ Mid (Theory) Q. Solve/Spring 2023/Q5_a.c
@@ -1,1 +1,31 @@
-// Solution in the README file 
+#include <stdio.h>
+
+int main()
+{
+    int arr[10] = {0};
+    int k = 15;
+    for (int i = 1; i < 6; i += 2)
+    {
+        arr[i] = ++k - 2;
+        k++;
+    }
+    int c = 0;
+    for (int i = 6; i < 10; i++)
+    {
+        for (int j = 10; j >= i; j--)
+        {
+            arr[j] = ++c;
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            arr[i] = ++k;
+        }
+    }
+
+    // printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d," ,arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9]);
+}
+
+// Solution in the README file
